3fc8182ae63783f0fae73422b46b8acd00658ade
Alex Tatomir <atatomir5@gmai.com>
2022-04-14 08:13:36 +0100

    scripts moved

---
+++ scripts/test/custom2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --job-name=test_rev_custom
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=atatomir5@gmail.com
+#SBATCH --partition=long
+#SBATCH --gres=gpu:1
+
+CONSTRAINTS="${HOME}/3D-RetinaNet/constraints/full"
+CLIP="10."
+CENTRALITY="custom2"
+CCN_CUSTOM_ORDER="7,40,38,23,39,27,11,33,28,5,22,21,24,20,31,16,26,15,9,6,18,34,19,30,25,3,12,2,10,36,8,35,4,32,1,37,29,14,13,17,0"
+
+#VAL_SUBSETS="val_1"
+VAL_SUBSETS="test"
+
+EXP_NAME="resnet50I3D512-Pkinetics-b8s8x1x1-roadt1-h3x3x3-custom2c10.000000-04-02-11-07-56x"
+
+sh test.sh $CONSTRAINTS $CLIP $CENTRALITY $CCN_CUSTOM_ORDER $VAL_SUBSETS $EXP_NAME
---
+++ scripts/train/custom2.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --job-name=train_3D_Retinanet
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=atatomir5@gmail.com
+#SBATCH --partition=long
+#SBATCH --gres=gpu:8
+
+source ~/.bashrc
+source activate ccn
+
+ROOT="$HOME"
+DATASET="${ROOT}/road-dataset/"
+KINETICS="${ROOT}/3D-RetinaNet/kinetics-pt"
+
+CONSTRAINTS="${ROOT}/3D-RetinaNet/constraints/full"
+CLIP="10."
+
+OPTIM="SGD"
+CENTRALITY="custom2"
+CCN_CUSTOM_ORDER="7,40,38,23,39,27,11,33,28,5,22,21,24,20,31,16,26,15,9,6,18,34,19,30,25,3,12,2,10,36,8,35,4,32,1,37,29,14,13,17,0"
+CCN_NUM_CLASSES="41"
+BATCH_SIZE=8
+
+# CHECK ALL PARAMETERS!!!!!!!!
+
+cd ../..
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 NCCL_DEBUG="INFO" python main.py $DATASET $DATASET $KINETICS --MODE=train --ARCH=resnet50 --MODEL_TYPE=I3D --DATASET=road --TRAIN_SUBSETS=train_1 --VAL_SUBSETS=val_1 --SEQ_LEN=8 --TEST_SEQ_LEN=8 --BATCH_SIZE=$BATCH_SIZE --LR=0.0041 --CCN_CONSTRAINTS=$CONSTRAINTS --CCN_CENTRALITY=$CENTRALITY --CCN_CUSTOM_ORDER=$CCN_CUSTOM_ORDER --CCN_NUM_CLASSES=$CCN_NUM_CLASSES --OPTIM=$OPTIM --CLIP=$CLIP
